4b095c4ac914c2ab273fb75ea9a055a3e4bcf89c
gubali <masif_apps@live.com>
2024-11-08 11:13:33 +0530

    added code with js

--- max-nodejs/expresJs/app.js
+++ max-nodejs/expresJs/app.js
@@ -3,17 +3,18 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const expressHbs = require("express-handlebars");
 const app = express();
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layout",
-    defaultLayout: "master-layout",
-    extname: "hbs",
-  })
-);
-//app.set("view engine", "pug");
-app.set("view engine", "hbs");
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layout",
+//     defaultLayout: "master-layout",
+//     extname: "hbs",
+//   })
+// );
+app.set("view engine", "ejs");
 app.set("views", "views");
+// app.set("views", "hbs");
+//app.set("views", "views");
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: true }));
