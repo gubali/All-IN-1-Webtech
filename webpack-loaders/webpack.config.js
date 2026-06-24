b312411794099534c993accc6c9c7144880c0fae
gubali <masif_apps@live.com>
2026-06-24 09:59:51 +0530

    webpack handson

--- webpack-loaders/webpack.config.js
+++ webpack-loaders/webpack.config.js
@@ -1,6 +1,8 @@
 
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     mode: "production",
     //entry: "./index.js",
@@ -11,10 +13,36 @@ module.exports = {
     devtool: false,
     output: {
         filename: "[name].bundle.js",
+        //filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
         assetModuleFilename: "assets/[hash][ext]",
         clean: true
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            chunks: ["index"],
+            filename: 'index.html',
+            inject: "body",
+        }),
+        new HtmlWebpackPlugin({
+            template: './about.html',
+            chunks: ["explore"],
+            filename: './about.html',
+            inject: "body",
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, './assets'),
+                    to: path.resolve(__dirname, "dist", './assets/images')
+                }
+            ]
+        })
+    ],
     module: {
         rules: [{
             test: /.(jpeg|png|jpg|png)$/,
@@ -22,11 +50,12 @@ module.exports = {
         },
         {
             test: /\.(css)$/,
-            use: ["style-loader", "css-loader"]
+            // use: ["style-loader", "css-loader"]
+            use: [MiniCssExtractPlugin.loader, "css-loader"]
         },
         {
             test: /\.(scss)$/,
-            use: ["style-loader", "css-loader", "sass-loader"]
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
         },
         {
             test: /.(ttf)$/,
